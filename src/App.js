5f2bff47ef4326732effec67d25b1cfe05d73f63
tanay-star <banerjeetanay333@gmail.com>
2020-08-03 20:41:42 +0530

    started the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+//importing components
+import HomePage from "./homepage.js";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
