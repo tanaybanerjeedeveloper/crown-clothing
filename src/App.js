5c4b2785b31179dda2963e51de50500ee3ebb266
tanay-star <banerjeetanay333@gmail.com>
2020-08-06 19:25:16 +0530

    added routing and styled up the menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom"; //this the library for 'routing'
 import "./App.css";
 //importing components
 import HomePage from "./pages/homepage/homepage";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+}; // just creating a 'HatsPage' component locally in 'App' component
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { withRouter } from "react-router-dom"; //'withRouter' is a Higher Order Component. That means it is a compoenent that takes another component as a parameter and resturns a 'powered-up' component having the same name as the previous component.'withRouter' is a HOD component that gives its wrapping component access to 'location,match and history' props req for routing.
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
     <div
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
       className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }} //******
+      ></div>
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
+
+//* this div is applied because when we are going to hover over the backgrnd-img we don't want the image to exceed the div. We are applying the div over the 'content' div and not surrounding the 'content' div becz we want the iamge to expand on hover and not the 'content div'.
