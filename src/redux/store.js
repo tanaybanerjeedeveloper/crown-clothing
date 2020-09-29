f1e4f85a04677ab349eb247d9c960d070723bbcd
Tanay Banerjee <banerjeetanay333@gmail.com>
2020-09-29 15:47:09 +0530

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import { createStore, applyMiddleware } from 'redux'
 //adding persistence
 import { persistStore } from 'redux-persist'
 
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
